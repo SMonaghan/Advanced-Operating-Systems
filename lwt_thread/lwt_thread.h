86a4dcc527d8738b568b10f1c896ebdbc8cf0429
SMonaghan <Stephenjmonaghan@gmail.com>
2015-02-05 23:03:26 -0500

    Added dispatch and did a test file

--- lwt_thread/lwt_thread.h
+++ lwt_thread/lwt_thread.h
@@ -10,9 +10,9 @@
 unsigned int * active_thread_count = 0;
 
 typedef struct thread {
-	void *stack;
+	void * stack;
 	unsigned int id;
-	unsigned int sp;
+	void * sp;
 	struct thread *parent;
 	struct thread *child;
 	void *value;
@@ -40,20 +40,29 @@ int lwt_yield(lwt_t thread);
 int lwt_id(lwt_t thread);
 int lwt_info(lwt_info_t t);
 
+
 int generate_id(void){
 	//active_thread_count = mmap(NULL, sizeof(unsigned int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
 	*active_thread_count = *active_thread_count + 1;
 	return *active_thread_count;
 }
 lwt_t lwt_create(lwt_fn_t fn, void *data){
+	printf("Start\n");	
 	lwt_t lwt_thd = malloc(sizeof(struct thread));
+	printf("Malloced!!!!!!!\n");	
 	
 	lwt_thd->stack = malloc(STACK_SIZE);
-	lwt_thd->id = generate_id();
-	lwt_thd->sp = (unsigned int)&lwt_thd->stack;
-	lwt_thd->parent = lwt_current();
+	printf("Stack Allocated!!!!!!!\n");	
+	lwt_thd->id = 1;//generate_id();
+	printf("ID Generated!!!!!!!\n");	
+	lwt_thd->sp = (unsigned int) lwt_thd->stack + STACK_SIZE;
+	printf("Stack Pointer set!!!!!!!\n");	
+	//lwt_thd->parent = lwt_current();
+	//printf("Parent Decided!!!!!!!\n");	
 	lwt_thd->child = NULL;
+	printf("Child Set!!!!!!!\n");	
 	lwt_thd->value = NULL;
+	printf("Value Set!!!!!!!\n");	
 
 	return lwt_thd;
 }
@@ -91,11 +100,36 @@ void __lwt_schedule(void){
 
 }
 
-void __lwt_dispatch(lwt_t next, lwt_t current){
+void __lwt_dispatch(lwt_t current, lwt_t next){
 
+	printf("PUSH START!!!!!!!!!!");
+	asm volatile ("pushl %eax \n\t"
+			"pushl %ebx \n\t"
+			"pushl %ecx \n\t"
+			"pushl %edx \n\t"
+			"pushl %esi \n\t"
+			"pushl %edi \n\t");
+	printf("PUSH DONE!!!!!!!!!!");
+
+	printf("Stack Switch START!!!!!!!!!!");
+	asm volatile ("movl %%esp, %0 \n\t"
+			"movl %1, %%esp \n\t" :
+			"=r" (current->sp):
+			"r" (next->sp):
+			"memory");
+	printf("Stack Switch DONE!!!!!!!!!!");
+	
+	printf("POP START!!!!!!!!!!");
+	asm volatile ("popl %edi \n\t"
+			"popl %esi \n\t"
+			"popl %edx \n\t"
+			"popl %ecx \n\t"
+			"popl %ebx \n\t"
+			"popl %eax \n\t");
+	printf("POP DONE!!!!!!!!!!");
 }
 
-void* __lwt_stack_get(void){
+void * __lwt_stack_get(void){
 
 }
 
--- lwt_thread/test.c
+++ lwt_thread/test.c
@@ -2,6 +2,49 @@
 #include <stdio.h>
 #include "lwt_thread.h"
 
+void * fuck_you_func(void *d){
+	return 90;
+}
+
 int main(int argc, char argv[]){
+
+	printf("start\n");
+	lwt_t main = lwt_create(fuck_you_func, NULL);
+	printf("main created\n");
+	lwt_t test1 = lwt_create(fuck_you_func, NULL);
+	lwt_t test2 = lwt_create(fuck_you_func, NULL);
+
+	printf("all created!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
+	
+	//test1->stack = malloc(4096);
+	//test2->stack = malloc(4096);
+	
+	__lwt_dispatch(main, test1);
+
+	printf("Dispatch one\n");
+	printf("Dispatch one\n");
+	int a = 5;
+	int b = 10;
+	int c = a*b;
+
+	for(a=5; a<10; a++){
+		c+=b;
+	}
+
+	__lwt_dispatch(test1, test2);
+
+	printf("Dispatch two\n");
+	int x = 5;
+	int y = 10;
+	int z = a*b;
+
+	for(x=5; x<10; x++){
+		z+=y;
+	}
+	printf("%d %d %d\n", x, y, z);
+
+	__lwt_dispatch(test2, test1);
+	printf("Dispatch three\n");
+	printf("%d %d %d\n", a, b, c);
 	printf("Done\n");
 }
