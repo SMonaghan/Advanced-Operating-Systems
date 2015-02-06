8ff3d515609e233d3a93ec85148efab88c74ee7b
SMonaghan <Stephenjmonaghan@gmail.com>
2015-02-05 21:18:23 -0500

    Made some changes to the struct

--- lwt_thread/lwt_thread.h
+++ lwt_thread/lwt_thread.h
@@ -4,17 +4,17 @@
 #include <sys/mman.h>
 //#include <unistd.h>
 
-#define STACK_SIZE (16 * 1024)//Size of the stack
+#define STACK_SIZE (4 * 1024)//Size of the stack
 #define LWT_NULL NULL//This is for the scheduling
 
 unsigned int * active_thread_count = 0;
 
 typedef struct thread {
-	void *mem;
+	void *stack;
 	unsigned int id;
 	unsigned int sp;
-	struct lwt *parent;
-	struct lwt *child;
+	struct thread *parent;
+	struct thread *child;
 	void *value;
 } *lwt_t;
 
@@ -24,6 +24,7 @@ typedef enum {
 	LWT_INFO_NTHD_ZOMBIES,
 } lwt_info_t;
 
+static lwt_t active_thread;
 
 void __lwt_schedule(void);
 void __lwt_dispatch(lwt_t next, lwt_t current);
@@ -39,20 +40,17 @@ int lwt_yield(lwt_t thread);
 int lwt_id(lwt_t thread);
 int lwt_info(lwt_info_t t);
 
-//unsigned int * count = mmap(NULL, sizeof(unsigned int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-
-
 int generate_id(void){
-	active_thread_count = mmap(NULL, sizeof(unsigned int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+	//active_thread_count = mmap(NULL, sizeof(unsigned int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
 	*active_thread_count = *active_thread_count + 1;
 	return *active_thread_count;
 }
 lwt_t lwt_create(lwt_fn_t fn, void *data){
 	lwt_t lwt_thd = malloc(sizeof(struct thread));
 	
-	lwt_thd->mem = malloc(STACK_SIZE);
+	lwt_thd->stack = malloc(STACK_SIZE);
 	lwt_thd->id = generate_id();
-	lwt_thd->sp = (unsigned int)&lwt_thd->mem;
+	lwt_thd->sp = (unsigned int)&lwt_thd->stack;
 	lwt_thd->parent = lwt_current();
 	lwt_thd->child = NULL;
 	lwt_thd->value = NULL;
@@ -73,7 +71,7 @@ int lwt_yield(lwt_t thread){
 }
 
 lwt_t lwt_current(void){
-
+	return active_thread;
 }
 
 int lwt_id(lwt_t thread){
@@ -85,6 +83,11 @@ int lwt_info(lwt_info_t t){
 }
 
 void __lwt_schedule(void){
+	lwt_t next_thread = active_thread;
+	/*next_thread = ?*/
+	/*put stuff here*/
+	active_thread = next_thread;
+
 
 }
 
