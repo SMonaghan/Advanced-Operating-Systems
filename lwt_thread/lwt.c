74967c52a84bcfff6e34a73444463f136efca8a3
SMonaghan <Stephenjmonaghan@gmail.com>
2015-04-21 19:58:52 -0400

    Everything is up to date now, everything has changed

--- lwt_thread/lwt.c
+++ lwt_thread/lwt.c
@@ -1,155 +1,187 @@
 #include "lwt.h"
 
-volatile lwt_t rq_head, rq_tail;
-lwt_t pool_head;
-void* start;
-unsigned int global_id;
+__thread struct lwt_ktcb* ktcb;
+__thread lwt_t rq_head;
+__thread lwt_t pool_head, temp_pool;
+__thread void* start;
+__thread unsigned int global_id, n_chan;
 
 int main(void);
 
-void __attribute__ ((constructor)) lwt_init(){
+void __attribute__ ((constructor)) __lwt_init(){
 	__lwt_stack_create();
 	global_id = 1;
-	lwt_t maint = lwt_create(main, NULL);
+	n_chan = 0;
+	lwt_t maint = __lwt_stack_get();
+	maint->id = global_id;
+	global_id++;
+	maint->function = main;
+	maint->flags = LWT_NOJOIN;
+	maint->owner = ktcb;
+	
+	ktcb = malloc(sizeof(struct lwt_ktcb));
+	ktcb->wf_rb = __lwt_rb_create(WAIT_FREE_RB_SIZE);
+	pthread_cond_init(&(ktcb->block_var), NULL);
+	pthread_mutex_init(&(ktcb->block_mutex), NULL);
+
+	rq_head = maint->next = maint->prev = maint;
+	maint->state = RUNNABLE;
+	lwt_create(__lwt_idle, NULL, LWT_NOJOIN);
+	printf("here rq: %d, next: %d, maint: %d\n", rq_head->id, rq_head->next->id, maint->id);
 	__lwt_schedule();
 }
-
-lwt_t lwt_create(lwt_fn_t fn, void *data){
+int lwt_kthd_create(lwt_fn_t fn, lwt_chan_t c){
+	struct lwt_kthd_init* new_init = malloc(sizeof(struct lwt_kthd_init));
+	new_init->fn = fn;
+	new_init->c = c;
+	
+	int ret = pthread_create(&(new_init->id), NULL, __lwt_kthd_init, new_init);
+	pthread_detach(new_init->id);
+	if(ret!=0) return -1;
+	return 0;
+}
+void __lwt_kthd_init(struct lwt_kthd_init* new_init){
+	__lwt_init();
+	ktcb->id = new_init->id;
+	rq_head->function = new_init->fn;
+	rq_head->param = new_init->c;
+	new_init->c->snd_cnt++;
+	free(new_init);
+	lwt_yield(LWT_NULL);
+}
+void __lwt_idle(){
+	while(1){
+		if(ktcb->wf_rb->tail > ktcb->wf_rb->head){
+			//consume
+		}
+		if(rq_head->next == rq_head){
+			//block
+		}
+		lwt_yield(LWT_NULL);
+	}
+}
+lwt_t lwt_create(lwt_fn_t fn, void *data, lwt_flags_t flags){
 	lwt_t lwt_thd = __lwt_stack_get();
+	if(temp_pool) __lwt_pool_merge();
 	lwt_thd->id = global_id;
 	global_id++;
-	void* sp = (int) lwt_thd + STACK_SIZE - 4;
 	
 	lwt_thd->param = data;
 	lwt_thd->function = fn;
 	lwt_thd->ret = LWT_NULL;
 	lwt_thd->joiner = LWT_NULL;
+	lwt_thd->flags = flags;
+	lwt_thd->owner = ktcb;
 	
-	*((int*) sp-1) = __lwt_trampoline;
-	*((int*) sp-2) = 0;
-
-	lwt_thd->sp = sp - 4;
-	__lwt_enqueue(&rq_head, &rq_tail, lwt_thd);
-
+	__lwt_list_add(rq_head->prev, rq_head, lwt_thd);
 	lwt_thd->state = RUNNABLE;
 	return lwt_thd;
 }
-
+lwt_t lwt_create_chan(lwt_chan_fn_t fn, lwt_chan_t c, lwt_flags_t flags){
+	lwt_t lwt = lwt_create(fn, c, flags);
+	c->snd_cnt++;
+	return lwt;
+}
 void * lwt_join(lwt_t thread){
 	lwt_t current = lwt_current();
-	
+	void* ret;
 	if(current == thread) return NULL;
-	if(thread->state == ZOMBIE) goto zombie;	
-	
-	assert(__lwt_dequeue(&rq_head, &rq_tail) == current);
-	thread->joiner = current;
-	current->state = BLOCKED;
-	__lwt_schedule();
-	end:
+	if(thread->state == ZOMBIE){
+		ret = thread->ret;
+		thread->state = DEAD;
+	} else {
+		thread->joiner = current;
+		ret = __lwt_block(current, BLOCKED);
+	}
 	__lwt_stack_return(thread);
-	return current->joinret;
-
-	zombie:
-	current->joinret = thread->ret;
-	thread->state = DEAD;
-	goto end;
+	return ret;
 }
-
 void lwt_die(void * ret){
 	lwt_t current = lwt_current();
-	if(current->joiner == LWT_NULL) goto zombie;
-
-	current->joiner->joinret = ret;
-	__lwt_enqueue(&rq_head, &rq_tail, current->joiner);	
-	current->joiner->state = RUNNABLE;
 	current->state = DEAD;
-	end:
-	if(rq_head->next == rq_head) exit(0);
-	__lwt_dequeue(&rq_head, &rq_tail);
-	__lwt_schedule();
+	if(__builtin_expect(current->flags, 0)) goto nojoin;
+	if(current->joiner == LWT_NULL) current->state = ZOMBIE;
+	else __lwt_unblock(current->joiner, ret);
 	
-	zombie:
-	current->state = ZOMBIE;
+	end:
+	if(current->id == 1){
+		free(start);
+		pthread_exit(ret);
+	}
+	__lwt_list_rem(rq_head->prev, rq_head->next);
+	lwt_yield(LWT_NULL);
+
+	nojoin:
+	current->next = temp_pool;
+	temp_pool = current;
 	goto end;
 }
+int lwt_info(lwt_info_t t){
+	int active=0, blocked=0, zombies=0, snd=0, rcv=0, i=0;
 
+	for(i = 0; i<MAX_THREADS; i++){
+		if(((lwt_t) (start+i*STACK_SIZE))->state == RUNNABLE) active++;
+		if(((lwt_t) (start+i*STACK_SIZE))->state == BLOCKED) blocked++;
+		if(((lwt_t) (start+i*STACK_SIZE))->state == ZOMBIE) zombies++;
+		if(((lwt_t) (start+i*STACK_SIZE))->state == SND_BLK) snd++;
+		if(((lwt_t) (start+i*STACK_SIZE))->state == RCV_BLK) rcv++;
+	}
+	if(t == LWT_INFO_NTHD_RUNNABLE) return active;
+	if(t == LWT_INFO_NTHD_BLOCKED)	return blocked;
+	if(t == LWT_INFO_NTHD_ZOMBIES)	return zombies;
+	if(t == LWT_INFO_NSNDING) 	return snd;
+	if(t == LWT_INFO_NRCVING)	return rcv;
+	if(t == LWT_INFO_NCHAN)		return n_chan;
+	return 0; 
+}
 lwt_chan_t lwt_chan(int sz){
 	lwt_chan_t chan = malloc(sizeof(struct lwt_channel));
-	chan->snd_cnt = 0;
-	chan->rcv_cnt = 0;
-	lwt_current()->recv_chan = chan;
+	chan->next = chan->prev = chan;
+	chan->snd_cnt = chan->rcv_deref = chan->group = chan->mark = chan->buf = 0;;
+	chan->block_q_head = malloc(sizeof(struct lwt_tcb));
+	chan->block_q_head->next = chan->block_q_head->prev = chan->block_q_head;
 	chan->receiver = lwt_current();
-	return chan;
+	chan->owner = ktcb;
+	n_chan++;
+	if(sz > 0) chan->buf = __lwt_rb_create(sz);
+	return chan;	
 }
-
 void lwt_chan_deref(lwt_chan_t c){
-	
-	lwt_t current = lwt_current();
-	if(c->receiver==current) c->rcv_cnt = -1;
-	else{
-		if(current->snd_next==NULL) return;
-
-		c->snd_cnt--;
-		current->snd_prev->snd_next = current->snd_next;
-		current->snd_next->snd_prev = current->snd_prev;
-	}
-	if(c->snd_cnt <= 0 && c->rcv_cnt == -1) free(c);	
+	if(c->receiver == lwt_current()) c->rcv_deref = 1;
+	else c->snd_cnt--;
+	if(c->snd_cnt > 0 || !c->rcv_deref) return;
+	if(c->buf) __lwt_rb_destroy(c->buf);
+	free(c);
+	n_chan--;
 }
-
-int lwt_snd(lwt_chan_t c, void* data){
-	assert(data != NULL);	
-	if(c->rcv_cnt == -1) return -1;
-
-	if(c->rcv_cnt == 1){
-		c->rcv_data = data;
-		c->rcv_cnt = 0;
-		__lwt_enqueue(&rq_head, &rq_tail, c->receiver);
-		c->receiver->state = RUNNABLE;
-		//yield?
-	}else if(c->rcv_cnt == 0){
-		lwt_t current = lwt_current();
-		current->snd_data = data;
-		__lwt_dequeue(&rq_head, &rq_tail);
-		__lwt_enqueue(&(c->block_q_head), &(c->block_q_tail), current);
-		current->state = BLOCKED;
-		__lwt_schedule(); //or directed yield?
-	}
+lwt_cgrp_t lwt_cgrp(){
+	lwt_cgrp_t grp = malloc(sizeof(struct lwt_chan_group));
+	grp->chan_cnt = 0;
+	grp->event_q_head = malloc(sizeof(struct lwt_channel));
+	grp->event_q_head->next = grp->event_q_head->prev = grp->event_q_head;
+	grp->owner = ktcb;
+	return grp;
+}
+int lwt_cgrp_free(lwt_cgrp_t grp){
+	if(grp->chan_cnt > 0) return -1;
+	free(grp);
 	return 0;
 }
-
-void* lwt_rcv(lwt_chan_t c){
-	if(c->snd_cnt == 0) return NULL;
-	if(c->block_q_head != NULL){ //assume this is how it works
-		lwt_t sender = __lwt_dequeue(&(c->block_q_head), &(c->block_q_tail));
-		__lwt_enqueue(&rq_head, &rq_tail, sender);
-		sender->state = RUNNABLE;
-		return sender->snd_data;
-	}else{
-		lwt_t current =	__lwt_dequeue(&rq_head, &rq_tail);
-		current->state = BLOCKED;
-		c->rcv_cnt = 1;
-		__lwt_schedule();
-		return c->rcv_data;
-	}
+int lwt_cgrp_add(lwt_cgrp_t grp, lwt_chan_t c){
+	if(c->group) return -1;
+	if(c->event) __lwt_list_add(grp->event_q_head->prev, grp->event_q_head, c);
+	grp->chan_cnt++;
+	c->group = grp;
+	return 0;
 }
-
-int lwt_snd_chan(lwt_chan_t c, lwt_chan_t sending){
-	sending->snd_cnt++;
-	return lwt_snd(c, sending);
+int lwt_cgrp_rem(lwt_cgrp_t grp, lwt_chan_t c){
+	if(c->group != grp) return -1;
+	if(c->event) return 1;
+	grp->chan_cnt--;
+	c->group = NULL;
+	return 0;
 }
-
-lwt_chan_t lwt_rcv_chan(lwt_chan_t c){
-	lwt_chan_t rcving = lwt_rcv(c);
-	lwt_t current = lwt_current();
-	__lwt_enqueue(&(rcving->senders_head), &(rcving->senders_tail), current);
-	c->snd_cnt += 1;
-	return rcving;
-}
-
-lwt_t lwt_create_chan(lwt_chan_fn_t fn, lwt_chan_t c){
-	lwt_t lwt = lwt_create(fn, NULL);
-	lwt->recv_chan = c;
-	__lwt_enqueue(&(c->senders_head), &(c->senders_tail), lwt);
-	c->snd_cnt += 1;
-	return lwt;
+lwt_chan_t lwt_cgrp_wait (lwt_cgrp_t grp){
+	if(grp->event_q_head == grp->event_q_head->next) __lwt_block(lwt_current(), GRP_BLK);
+	return grp->event_q_head->next;	
 }
--- lwt_thread/lwt.h
+++ lwt_thread/lwt.h
@@ -1,10 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
-
-#define STACK_SIZE (8 * 4096)
+#include <pthread.h>
+#define STACK_SIZE (32 * 4096)
 #define LWT_NULL NULL
 #define MAX_THREADS 256
+#define WAIT_FREE_RB_SIZE 256
 
 typedef enum {
 	LWT_INFO_NTHD_RUNNABLE,
@@ -18,129 +19,227 @@ typedef enum {
 typedef enum {
 	RUNNABLE,
 	BLOCKED,
+	GRP_BLK,
+	SND_BLK,
+	RCV_BLK,
 	ZOMBIE,
 	DEAD
 } lwt_state_t;
 
-typedef void *(*lwt_fn_t)(void *);
-typedef struct lwt_tcb *lwt_t;
-typedef struct lwt_channel *lwt_chan_t; 
+typedef enum {
+	LWT_JOIN,
+	LWT_NOJOIN
+} lwt_flags_t;
 
-typedef struct lwt_tcb {
+typedef void *(*lwt_fn_t)(void *);
+typedef void *(*lwt_chan_fn_t)(lwt_chan_t);
+typedef struct lwt_tcb* lwt_t;
+typedef struct lwt_ktcb* lwt_kthd_t;
+typedef struct lwt_channel* lwt_chan_t;
+typedef struct lwt_chan_group* lwt_cgrp_t;
+
+struct lwt_tcb {
+	struct lwt_tcb *next, *prev, *joiner;
 	unsigned int id;
-	volatile void *sp, *joinret;
-	struct lwt_tcb *next, *prev, *joiner, *snd_next, *snd_prev;
-	void *param, *ret, *snd_data;
-	lwt_chan_t recv_chan;
+	void *sp, *blk_data;
+	void *param, *ret;
 	lwt_state_t state;
 	lwt_fn_t function;
-} *lwt_t;
+	lwt_flags_t flags;
+	lwt_kthd_t owner;
+};
+struct lwt_channel {
+	struct lwt_channel *next, *prev;
+	lwt_t block_q_head, receiver;
+	int snd_cnt, rcv_deref, event;
+	void* mark;
+	lwt_cgrp_t group;
+	struct ring_buf* buf;
+	lwt_kthd_t owner;
+};
+struct lwt_chan_group {
+	int chan_cnt;
+	lwt_chan_t event_q_head;	
+	lwt_kthd_t owner;
+};
+struct ring_buf {
+	void** buffer;
+	unsigned int head, tail, size, count;
+};
+struct lwt_ktcb {
+	pthread_t id;
+	volatile struct ring_buf* wf_rb;
+	pthread_cond_t block_var;
+	pthread_mutex_t block_mutex;
+};
+struct lwt_kthd_init {
+	pthread_t id;
+	lwt_fn_t fn;
+	lwt_chan_t c;
+};
 
-typedef struct lwt_channel {
-	int snd_cnt;
-	lwt_t senders_head, senders_tail;
-	lwt_t block_q_head, block_q_tail;
-	void* rcv_data;
-	int rcv_cnt;
-	lwt_t receiver;
-} *lwt_chan_t;
+extern __thread lwt_kthd_t ktcb;
+extern __thread lwt_t rq_head;
+extern __thread lwt_t pool_head, temp_pool;
+extern __thread void* start;
+extern __thread unsigned int global_id, n_chan;
 
-typedef void *(*lwt_chan_fn_t)(lwt_chan_t);
+lwt_chan_t 		lwt_chan	(int sz);
+void 	   		lwt_chan_deref	(lwt_chan_t c);
+lwt_t			lwt_create_chan	(lwt_chan_fn_t fn, lwt_chan_t c, lwt_flags_t flags);
+static inline void	lwt_chan_mark_set(lwt_chan_t c, void* mark);
+static inline void*	lwt_chan_mark_get(lwt_chan_t c);
+static inline int 	lwt_snd		(lwt_chan_t c, void* data);
+static inline void* 	lwt_rcv		(lwt_chan_t c);
+static inline int 	lwt_snd_chan	(lwt_chan_t c, lwt_chan_t sending);
+static inline lwt_chan_t lwt_rcv_chan	(lwt_chan_t c);
+static inline void	__lwt_snd_block (lwt_chan_t c, void* data);
+static inline void*	__lwt_rcv_block (lwt_chan_t c);
+static inline int	__lwt_snd_sync  (lwt_chan_t c, void* data);
+static inline void*	__lwt_rcv_sync  (lwt_chan_t c);
+static inline void	__lwt_snd_async (lwt_chan_t c, void* data);
+static inline void*	__lwt_rcv_async (lwt_chan_t c);
 
-extern volatile lwt_t rq_head, rq_tail;
-extern lwt_t pool_head;
-extern void* start;
-extern unsigned int global_id;
+lwt_cgrp_t 		lwt_cgrp	(void);
+int 	   		lwt_cgrp_free	(lwt_cgrp_t grp);
+int 	   		lwt_cgrp_add	(lwt_cgrp_t grp, lwt_chan_t c);
+int 	   		lwt_cgrp_rem	(lwt_cgrp_t grp, lwt_chan_t c);
+lwt_chan_t 		lwt_cgrp_wait	(lwt_cgrp_t grp);
+static inline void	__lwt_cgrp_notify(lwt_chan_t c);
+static inline void	__lwt_cgrp_denotify(lwt_chan_t c); 
 
-void * lwt_join(lwt_t thread);
-void lwt_die(void * ret);
-lwt_t lwt_create(lwt_fn_t fn, void *data);
+int 			lwt_kthd_create (lwt_fn_t fn, lwt_chan_t c);
+void			__lwt_kthd_init (struct lwt_kthd_init* new_init);
+void			__lwt_idle	(void);
 
-lwt_chan_t lwt_chan(int sz);
-void lwt_chan_deref(lwt_chan_t c);
-int lwt_snd(lwt_chan_t c, void* data);
-void* lwt_rcv(lwt_chan_t c);
-int lwt_snd_chan(lwt_chan_t c, lwt_chan_t sending);
-lwt_chan_t lwt_rcv_chan(lwt_chan_t c);
-lwt_t lwt_create_chan(lwt_chan_fn_t fn, lwt_chan_t c);
+void* 	   		lwt_join	(lwt_t thread);
+void 	   		lwt_die		(void * ret);
+lwt_t 	   		lwt_create	(lwt_fn_t fn, void *data, lwt_flags_t flags);
+int 			lwt_info	(lwt_info_t t);
+static inline lwt_t 	lwt_current	(void);
+static inline int 	lwt_yield	(lwt_t thread);
+static inline int 	lwt_id		(lwt_t thread);
+static inline void 	__lwt_schedule	(void);
+static inline void 	__lwt_trampoline(void);
+static inline void 	__lwt_dispatch	(lwt_t next);
+static inline lwt_t 	__lwt_stack_get	(void);
+static inline void 	__lwt_stack_return(void *stk);
+static inline void 	__lwt_stack_create(void);
+static inline void 	__lwt_pool_merge(void);
+static inline void* 	__lwt_block	(lwt_t thread, lwt_state_t state);
+static inline void 	__lwt_unblock	(lwt_t thread, void* data);
+static inline void 	__lwt_list_add	(void* prev, void* next, void* insert);
+static inline void 	__lwt_list_rem	(void* prev, void* next);
+static inline void	__lwt_list_enq_s(void** q_head, void* insert);
+static inline void	__lwt_list_deq_s(void** q_head);
+static inline int 	__lwt_rb_add	(struct ring_buf* buf, void* value);
+static inline void* 	__lwt_rb_rem	(struct ring_buf* buf);
+static inline int	__lwt_rb_add_wf (struct ring_buf* buf, void* value);
+static inline void* 	__lwt_rb_rem_wf (struct ring_buf* buf);
+static inline struct ring_buf* __lwt_rb_create(int sz);
+static inline void	__lwt_rb_destroy(struct ring_buf* buf);
 
-static inline lwt_t lwt_current(void);
-static inline int lwt_yield(lwt_t thread);
-static inline int lwt_id(lwt_t thread);
-static inline int lwt_info(lwt_info_t t);
+static inline void lwt_chan_mark_set(lwt_chan_t c, void* mark){ c->mark = mark; }
+static inline void* lwt_chan_mark_get(lwt_chan_t c){ return c->mark; }
+static inline void __lwt_schedule(void){ __lwt_dispatch(rq_head); }
+static inline int lwt_id(lwt_t thread){	return thread->id; }
 
-static inline void __lwt_schedule(void);
-static inline void __lwt_trampoline(void);
-static inline void __lwt_dispatch(lwt_t next);
-static inline void * __lwt_stack_get(void);
-static inline void __lwt_stack_return(void *stk);
-static inline void __lwt_stack_create(void);
 
-static inline void __lwt_enqueue(lwt_t *head, lwt_t *tail, lwt_t insert);
-static inline lwt_t __lwt_dequeue(lwt_t *head, lwt_t *tail);
+static inline int lwt_snd_chan(lwt_chan_t c, lwt_chan_t sending){
+	sending->snd_cnt++;
+	return lwt_snd(c, sending);
+}
+static inline lwt_chan_t lwt_rcv_chan(lwt_chan_t c){ return lwt_rcv(c); }
 
+static inline lwt_t lwt_current(void){
+	int offset;
+	return ((int) &offset & 0xfffe0000);
+}
 static inline int lwt_yield(lwt_t thread){
-	rq_tail = rq_head;	
 	rq_head = rq_head->next;
-
-	if(thread == LWT_NULL || thread == rq_head) goto finish;	
-	if(thread->state != RUNNABLE || thread == lwt_current()) goto fail;
-
-	thread->prev->next = thread->next;
-	thread->next->prev = thread->prev;
-	
-	rq_tail->next = thread;
-	thread->prev = rq_tail;
-	thread->next = rq_head;
-	rq_head->prev = thread;
-	rq_head = thread;
+	if(__builtin_expect(thread, LWT_NULL)) goto direct;
 	
 	finish:
 	__lwt_schedule();
 	return 1;
-
+	
+	direct:
+	if(thread == rq_head) goto finish;
+	if(thread->state!=RUNNABLE || thread==lwt_current()) goto fail;	
+	__lwt_list_rem(thread->prev, thread->next);
+	__lwt_list_add(rq_head->prev, rq_head, thread); 	
+	rq_head = thread;
+	goto finish;
+	
 	fail:
-	rq_head = rq_tail;
-	rq_tail = rq_tail->prev;
+	rq_head = rq_head->prev;
 	return 0;	
 }
-
-static inline lwt_t lwt_current(void){
-	int offset;
-	return ((int) &offset & 0xffff8000);
+static inline int lwt_snd(lwt_chan_t c, void* data){
+	assert(data);	
+	if(c->rcv_deref) return -1;
+	if(c->receiver->state == RCV_BLK) return __lwt_snd_sync(c, data);
+	if(c->group) __lwt_cgrp_notify(c);
+	c->event = 1;
+	if(c->buf) __lwt_snd_async(c, data);
+	else __lwt_snd_block(c, data);
+	return 0;
 }
-
-static inline int lwt_id(lwt_t thread){
-	return *((unsigned int*) thread);
+static inline void* lwt_rcv(lwt_chan_t c){
+	if(c->buf) return __lwt_rcv_async(c);
+	if(c->block_q_head == c->block_q_head->next) return __lwt_rcv_block(c);
+	return __lwt_rcv_sync(c);
 }
-
-static inline int lwt_info(lwt_info_t t){
-	int active = 0, blocked = 0, dead = 0, i = 0;
-
-	for(i = 0; i < MAX_THREADS; i++){
-		if(((lwt_t) (start+i*STACK_SIZE))->state == RUNNABLE) active++;
-		if(((lwt_t) (start+i*STACK_SIZE))->state == BLOCKED) blocked++;
-		if(((lwt_t) (start+i*STACK_SIZE))->state == ZOMBIE) dead++;
-	}
-	if(t == LWT_INFO_NTHD_RUNNABLE){
-		return active;
-	}
-	if(t == LWT_INFO_NTHD_BLOCKED){
-		return blocked;
-	}
-	if(t == LWT_INFO_NTHD_ZOMBIES){
-		return dead;
-	}
-	return 0; 
+static inline int __lwt_snd_sync(lwt_chan_t c, void* data){
+	__lwt_unblock(c->receiver, data);
+	return 0;
 }
-
-static inline void __lwt_schedule(void){
-	assert(rq_head!=NULL);
-	__lwt_dispatch(rq_head);
+static inline void* __lwt_rcv_sync(lwt_chan_t c){
+	lwt_t sender = c->block_q_head->next;
+	__lwt_list_rem(sender->prev, sender->next);
+	__lwt_unblock(sender, sender->blk_data);	
+	if(c->group && c->block_q_head == c->block_q_head->next) __lwt_cgrp_denotify(c);
+	return sender->blk_data;
 }
-
+static inline void __lwt_snd_async(lwt_chan_t c, void* data){
+	if(!__lwt_rb_add(c->buf, data)) __lwt_snd_block(c, data);
+}
+static inline void* __lwt_rcv_async(lwt_chan_t c){
+	void* ret = __lwt_rb_rem(c->buf);
+	if(!ret){
+		if(c->block_q_head != c->block_q_head->next) return __lwt_rcv_sync(c);
+		else return __lwt_rcv_block(c);
+	}
+	if(c->group && (c->block_q_head == c->block_q_head->next) && c->buf->count == 0) __lwt_cgrp_denotify(c);
+	return ret;
+}
+static inline void __lwt_snd_block(lwt_chan_t c, void* data){
+	lwt_t current = lwt_current();
+	__lwt_list_rem(rq_head->prev, rq_head->next);
+	rq_head = rq_head->next;
+	__lwt_list_add(c->block_q_head->prev, c->block_q_head, current);
+	current->state = SND_BLK;
+	current->blk_data = data;
+	__lwt_schedule();
+}
+static inline void* __lwt_rcv_block(lwt_chan_t c){
+	return __lwt_block(lwt_current(), RCV_BLK);
+}
+static inline void __lwt_cgrp_notify(lwt_chan_t c){
+	if(c->receiver->state == GRP_BLK) __lwt_unblock(c->receiver, c);
+	if(!c->event) __lwt_list_add(c->group->event_q_head->prev, c->group->event_q_head, c);
+}	
+static inline void __lwt_cgrp_denotify(lwt_chan_t c){
+	__lwt_list_rem(c->prev, c->next);
+	c->event = 0;
+}
+static inline void __lwt_trampoline(void){
+	lwt_t current = lwt_current();
+	current->ret = current->function(current->param);
+	lwt_die(current->ret);
+	assert(0);
+}
 static inline void __lwt_dispatch(lwt_t next){
- 
 	asm volatile (	"pushl %%eax \n\t"
 			"pushl %%ebx \n\t"
 			"pushl %%ecx \n\t"
@@ -150,9 +249,8 @@ static inline void __lwt_dispatch(lwt_t next){
 			"pushl %%ebp \n\t"
 			"push $1f \n\t" 
 			"movl %%esp, %%eax \n\t"
-			"movl %%eax, %%edx \n\t"
-			"and $0xffff8000, %%edx \n\t"
-			"movl %%eax, 0x4(%%edx) \n\t"
+			"and $0xfffe0000, %%eax \n\t"
+			"movl %%esp, 0x10(%%eax) \n\t"
 			"movl %0, %%esp \n\t"
 			"ret \n\t"
 			"1: \n\t"
@@ -165,60 +263,104 @@ static inline void __lwt_dispatch(lwt_t next){
 			"popl %%eax \n\t"
 			:	
 			:"g" (next->sp)
-			:"memory", "%esp", "%eax", "%ebx", "%ecx", "%edx", "%esi", "%esi", "%ebp");
+			:"memory","%esp", "%eax");
 }
-
-static inline void __lwt_trampoline(void){
-	volatile lwt_t current = lwt_current();
-
-	current->ret = current->function(current->param);
-	lwt_die(current->ret);
-	assert(0);
-}
-static inline void * __lwt_stack_get(void){
-	if(pool_head == NULL){
+static inline lwt_t __lwt_stack_get(void){
+	if(!pool_head){
 		printf("OUT OF FREE THREADS!\n");
 		return NULL;
 	}
-	void* ret = pool_head;
+	lwt_t ret = pool_head;
 	pool_head = pool_head->next;
+	ret->sp = (int) ret + STACK_SIZE - 8;
+	*((int*) ret->sp) = __lwt_trampoline;
 	return ret;
 }
-
 static inline void __lwt_stack_return(void *stk){
-	lwt_t ret = stk;
-	ret->next = pool_head;
-	ret->prev = NULL;
-	pool_head = ret;
+	((lwt_t) stk)->next = pool_head;
+	((lwt_t) stk)->prev = NULL;
+	pool_head = stk;
 }
 static inline void __lwt_stack_create(void){
-	void* allthatmemory = aligned_alloc(STACK_SIZE, MAX_THREADS * STACK_SIZE);
+	void* allthatmemory;
+	posix_memalign(&allthatmemory, STACK_SIZE, MAX_THREADS * STACK_SIZE);
 	int i = 0;
-	for(i = 0; i < MAX_THREADS; i++){
+	for(i = 0; i<MAX_THREADS; i++){
 		lwt_t current = allthatmemory + i*STACK_SIZE;
 		current->state = DEAD;
-		if(i == 0) current->next = NULL;
+		if(i==0) current->next = NULL;
 		else current->next = (int) current - STACK_SIZE;
 	}
-	pool_head = allthatmemory + (i - 1) * STACK_SIZE;
+	pool_head = allthatmemory + (i-1)*STACK_SIZE;
 	start = allthatmemory;
 }
-static inline void __lwt_enqueue(lwt_t* head, lwt_t *tail, lwt_t insert){
-	if(*head == NULL){
-		*head = insert;
-		insert->prev = insert;
-		*tail = insert;
-	} else insert->prev = (*head)->prev;
-
-	insert->next = *head;
-	(*tail)->next = insert;
-	(*head)->prev = insert;
-	*tail = insert;
+static inline void __lwt_pool_merge(void){
+	while(temp_pool){
+		lwt_t ret = temp_pool;
+		temp_pool = temp_pool->next;
+		__lwt_stack_return(ret);
+	}
 }
-static inline lwt_t __lwt_dequeue(lwt_t *head, lwt_t *tail){
-	lwt_t ret = *head;
-	*head = (*head)->next;
-	(*head)->prev = ret->prev;
-	(*tail)->next = *head;
+static inline void* __lwt_block(lwt_t thread, lwt_state_t state){
+	thread->state = state;
+	__lwt_list_rem(rq_head->prev, rq_head->next);
+	lwt_yield(LWT_NULL);
+	return thread->blk_data;
+}
+static inline void __lwt_unblock(lwt_t thread, void* data){
+	__lwt_list_add(rq_head->prev, rq_head, thread);
+	thread->state = RUNNABLE;
+	thread->blk_data = data;
+}
+static inline void __lwt_list_add(void* prev, void* next, void* insert){
+	((lwt_t) prev)->next = insert; 
+	((lwt_t) insert)->prev = prev;
+	((lwt_t) next)->prev = insert;
+	((lwt_t) insert)->next = next;
+}
+static inline void __lwt_list_rem(void* prev, void* next){
+	((lwt_t) prev)->next = next;
+	((lwt_t) next)->prev = prev;
+}
+static inline int __lwt_rb_add(struct ring_buf* buf, void* value){
+	if(buf->count == buf->size) return 0;
+	buf->buffer[buf->tail] = value;
+	buf->tail = (buf->tail+1)%(buf->size);
+	buf->count++;
+	return 1;
+}
+static inline void* __lwt_rb_rem(struct ring_buf* buf){
+	if(buf->count == 0) return 0;
+	void* ret = buf->buffer[buf->head];
+	buf->head = (buf->head+1)%(buf->size);
+	buf->count--;
 	return ret;
-} 
+}
+static inline struct ring_buf* __lwt_rb_create(int sz){
+	struct ring_buf* new_buf = malloc(sizeof(struct ring_buf));
+	new_buf->buffer = malloc(sizeof(void*)*sz);
+	new_buf->head = new_buf->tail = new_buf->count = 0;
+	new_buf->size = sz;
+	return new_buf;
+}
+static inline void __lwt_rb_destroy(struct ring_buf* buf){
+	free(buf->buffer);
+	free(buf);
+}
+static inline int __lwt_rb_add_wf(struct ring_buf* buf, void* value){
+	int old_tail = __sync_fetch_and_add(&(buf->tail), 1);
+	if((buf->tail-buf->head)%(buf->size) == 0){
+		buf->tail = old_tail;
+		return 0;
+	}
+	buf->buffer[(buf->tail)%(buf->size)] = value;
+	return 1;
+}
+static inline void* __lwt_rb_rem_wf(struct ring_buf* buf){
+	int old_head = __sync_fetch_and_add(&(buf->head), 1);	
+	if((buf->tail-old_head)%(buf->size) == 0){
+		buf->head = old_head;
+		return 0;
+	}
+	return buf->buffer[old_head%(buf->size)];
+}
